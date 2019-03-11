19164d5598dc6cf269ce1799859be4bce04bd1a8
bianyuanyuan <2714540821@qq.com>
2019-03-11 19:46:38 +0800

    js

---
+++ ConsoleTest/js/alert.js
@@ -0,0 +1,23 @@
+function taoLu(){
+    alert("怕了吗\n")
+    alert("呵呵\n");
+    alert("不要怕\n");
+    alert("少年\n");
+}
+alert("你是今天的幸运儿\n");
+alert("被上帝选中的命运之子\n");
+alert("接下来\n");
+alert("你的选择将决定你今后的人生\n");
+//  alert("呵呵\n");
+// alert("不要怕\n");
+// alert("少年\n");
+
+taoLu();
+alert("用心去感受\n");
+taoLu();
+alert("相信自己，不要放弃");
+taoLu();
+alert("这里有一个金斧头、银斧头、木斧头\n");
+alert("你选择哪个呢\n");
+alert("哈哈，无论你选择哪个\n");
+alert("你都是个小傻瓜\n");
